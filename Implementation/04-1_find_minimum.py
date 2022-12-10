f202c6bf4730d99139c7b404c1a0bbe819749fdb
Kaeun-Lee <lgeun7026@hanmail.net>
2022-12-10 17:27:25 +0900

    upload fine the minimum

---
+++ Implementation/04-1_find_minimum.py
@@ -0,0 +1,19 @@
+# 4-1. [선수 지식] 최솟값 구하기
+
+# Solution_1 -> python에서 가장 큰 값을 저장(무한대)
+arr = [5, 3, 7, 9, 2, 5, 2, 6]
+arrMin = float("inf")
+
+for x in arr:
+    if x < arrMin:
+        arrMin = x
+print(arrMin)
+
+
+# Solution_2 -> 첫 번째 값을 min으로 저장한 후 1번 index부터 비교
+arr = [5, 3, 7, 9, 2, 5, 2, 6]
+arrMin = arr[0]
+for i in range(1, len(arr)):
+    if arr[i] < arrMin:
+        arrMin = arr[i]
+print(arrMin)
