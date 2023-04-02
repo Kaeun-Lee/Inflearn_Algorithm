cd32e896fdf6208d486a3742a0c6b91c27c9c558
Kaeun-Lee <lgeun7026@hanmail.net>
2023-04-02 11:45:47 +0900

    add extra code

--- Implementation/04-1_find_minimum.py
+++ Implementation/04-1_find_minimum.py
@@ -13,7 +13,19 @@ print(arrMin)
 # Solution_2 -> 첫 번째 값을 min으로 저장한 후 1번 index부터 비교
 arr = [5, 3, 7, 9, 2, 5, 2, 6]
 arrMin = arr[0]
+
 for i in range(1, len(arr)):
     if arr[i] < arrMin:
         arrMin = arr[i]
 print(arrMin)
+
+
+# Solution_3
+arr = [5, 3, 7, 9, 2, 5, 2, 6]
+arr_min = float("inf")  # python에서 가장 큰 값
+
+for i in range(len(arr)):
+    if arr[i] < arr_min:
+        arr_min = arr[i]
+
+print(arr_min)
