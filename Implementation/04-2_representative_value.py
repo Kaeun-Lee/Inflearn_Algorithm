cf3a6c7bcc49ab28994ea524022f5664c26ac413
Kaeun-Lee <lgeun7026@hanmail.net>
2023-04-03 20:54:56 +0900

    add extra code

--- Implementation/04-2_representative_value.py
+++ Implementation/04-2_representative_value.py
@@ -9,22 +9,17 @@
 #        평균과의 최소 거리를 구하면서 거리가 같은 경우, 숫자가 클 때만 바꿔주면 가장 먼저 나온 학생 번호를 유지할 수 있음
 #        list comprehension으로 작성하는 습관을 들이자
 
-# Python의 round 함수는 round_half_even 방식을 택함. 즉, 반올림하는 자리의 숫자가 정확히 5일 때, 앞자리가 짝수가 되도록 반올림함
-# cf. 우리가 흔히 사용하는 반올림은 half_up 방식(5 이상은 올림)임
-a = 4.500
-b = 4.511
-c = 5.500
-d = int(66.5 + 0.5)  # 소수점이 5이상인 경우 1의 자릿수가 증가함
-
-print(round(a))  # 4
-print(round(b))  # 5
-print(round(c))  # 6
-print(d)  # 67
-
+# Python의 round()는 round_half_even 방식이므로, 반올림 계산 시 사용하면 X
+# round_half_even 방식 : 반올림하는 자리의 숫자가 정확히 5인 경우, 올림 하는 것이 아니라 "짝수"쪽에 근사 시킴
+# (ex) round(4.5) = 4, round(5.5) = 6, round(4.51) = 5
+# (cf) 우리가 흔히 사용하는 반올림은 half_up 방식(5 이상은 올림)임
+# 따라서 round(a) 대신, int(a + 0.5)를 사용할 것!
+# (ex) int(66.5 + 0.5) = 67 -> 소수점이 5 이상인 경우, 1의 자릿수가 증가
 
 # My_Solution -> (실패) 답을 맞히긴 했으나, round 함수로 인해 논리적 결함이 있음
 n = map(int, input())
 scores = list(map(int, input().split()))
+
 avg = int(round(sum(scores) / len(scores), 0))  # 평균값
 diff = float("inf")
 result = []
@@ -45,8 +40,10 @@ print(avg, result[0][1] + 1)
 # Solution_1 -> half up 반올림, 하나의 for문으로 처리
 n = int(input())
 a = list(map(int, input().split()))
-ave = int((sum(a) / n) + 0.5)
-min = 2147000000  # 정수형의 가장 큰 값(4byte일 경우, 2**31)
+
+ave = int((sum(a) / n) + 0.5)  # ave = round(sum(a) / n) -> (X)
+
+min = 2147000000  # int형으로 표현할 수 있는 (4byte = 2^31) 정도 되는 가장 큰 수
 
 # 평균과 가장 가까운 점수와 학생 번호 구하기
 for idx, x in enumerate(a):
@@ -56,7 +53,7 @@ for idx, x in enumerate(a):
         score = x
         res = idx + 1
     elif tmp == min:  # 같은 거리의 점수가 나왔을 때
-        if x > score:  # 동일한 점수가 나와도 먼저 나온 번호를 유지
+        if x > score:  # (중요) 동일한 점수가 나와도 먼저 나온 번호를 유지. 더 늦은 학생의 번호를 출력하려면, 등호(=) 추가
             score = x
             res = idx + 1
 print(ave, res)
@@ -76,16 +73,16 @@ print(mean_score, result[0][0] + 1)
 
 
 # test case 1
-# n : 10
-# scores : 45 73 66 87 92 67 75 79 75 80
+# n = 10
+# scores = 45 73 66 87 92 67 75 79 75 80
 # result : 74 7
 
 # test case 2
-# n : 15
-# scores : 12 34 17 6 11 15 27 42 39 31 25 36 35 25 17
+# n = 15
+# scores = 12 34 17 6 11 15 27 42 39 31 25 36 35 25 17
 # result : 25 11
 
 # test case 3
-# n : 10
-# scores : 12 34 17 6 11 15 27 42 39 31
+# n = 10
+# scores = 12 34 17 6 11 15 27 42 39 31
 # result : 23 7
