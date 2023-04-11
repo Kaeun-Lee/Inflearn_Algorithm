83c897b40bddc883214033c0d9bcc569db16fcb4
Kaeun-Lee <lgeun7026@hanmail.net>
2023-04-11 21:53:08 +0900

    edit minor things

--- Implementation/07_prime_number.py
+++ Implementation/07_prime_number.py
@@ -5,15 +5,16 @@
 # 조건 : 자연수의 개수 N(2 <= N <= 200,000)이 주어짐
 # 회고 : check 변수의 idx 번호가 1부터 n까지의 숫자라고 생각해야 함
 #        소수의 배수들은 1과 자기 자신뿐만 아니라 해당 소수를 약수로 가지고 있음
+#        while문과 for문으로 구하는 방법 모두 알아놓기
 
-# My_Solution
+# My_Solution -> (성공)
 import sys
 import math
 
 n = int(input())
-ch = [True for i in range(n + 1)]  # idx가 n까지 생기도록 생성
+ch = [True] * (n + 1)  # idx가 n까지 생기도록 생성
 
-for i in range(2, int(math.sqrt(n) + 1)):  # 1은 소수가 아님
+for i in range(2, int(math.sqrt(n)) + 1):  # 1은 소수가 아님
     if ch[i] == True:
         j = 2
         while i * j <= n:  # i의 배수
